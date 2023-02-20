2fee425075aaf36424d69f6a05fa60bb1fd5ca8e
abose <arun@core.ai>
2023-02-20 08:52:51 +0530

    chore: github get org details unit tests

--- src/github.js
+++ src/github.js
@@ -36,6 +36,7 @@ export function initGitHubClient() {
 export async function createIssue(owner, repo, title, body) {
     // https://docs.github.com/en/rest/issues/issues?apiVersion=2022-11-28#create-an-issue
     // {... "html_url": "https://github.com/octocat/Hello-World/issues/1347", ...}
+    console.log("create Github issue: ", arguments);
     let response = await octokit.request(`POST /repos/${owner}/${repo}/issues`, {
         owner,
         repo,
@@ -59,8 +60,7 @@ export async function createIssue(owner, repo, title, body) {
  * @return {Promise<{html_url:string}>}
  */
 export async function commentOnIssue(owner, repo, issueNumber, commentString) {
-    // https://docs.github.com/en/rest/issues/issues?apiVersion=2022-11-28#create-an-issue
-    // {... "html_url": "https://github.com/octocat/Hello-World/issues/1347", ...}
+    console.log("Comment on Github issue: ", arguments);
     let response = await octokit.request(`POST /repos/${owner}/${repo}/issues/${issueNumber}/comments`, {
         owner,
         repo,
@@ -75,27 +75,36 @@ export async function commentOnIssue(owner, repo, issueNumber, commentString) {
 }
 
 /**
- * Get the org details. Throws if org doesn't exist or is simple GitHub username passed.
+ * Get the org details or null if org doesn't exist
  * @param {string} org
  * @return {Promise<{is_verified:boolean, html_url:string, blog:string,
- * name:string, company:string}>} blog is the verified url for the org.
- * @throws if org doesn't exist
+ * name:string, company:string}> | null} blog is the verified url for the org. null if org doesnt exist
  */
 export async function getOrgDetails(org) {
     // https://docs.github.com/en/rest/issues/issues?apiVersion=2022-11-28#create-an-issue
     // {... "html_url": "https://github.com/octocat/Hello-World/issues/1347", ...}
-    let response = await octokit.request(`GET /orgs/${org}`, {
-        org
-    });
+    console.log("Get Org details: ", arguments);
+    try{
+        let response = await octokit.request(`GET /orgs/${org}`, {
+            org
+        });
 
-    let orgDetails = {
-        name: response.data.name,
-        company: response.data.company,
-        blog: response.data.blog,
-        is_verified: response.data.is_verified,
-        html_url: response.data.html_url
-    };
+        let orgDetails = {
+            name: response.data.name,
+            company: response.data.company,
+            blog: response.data.blog,
+            is_verified: response.data.is_verified,
+            html_url: response.data.html_url
+        };
 
-    console.log("getting org details: ", orgDetails);
-    return orgDetails;
+        console.log("GitHub org details: ", orgDetails);
+        return orgDetails;
+    } catch (e) {
+        if(e.status === 404){
+            console.log("no such org: ", org);
+            return null;
+        }
+        console.error("error getting github org: ", e);
+        throw e;
+    }
 }
